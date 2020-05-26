a932e07aec57be608ee0b52a071925f86d67bc50
Pramod Thomas <pmod23@gmail.com>
2020-05-26 22:21:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.each do |e|
-    if (valid_calls.include(e))
+    if (valid_calls.include?(e))
       return e
     end
   end
